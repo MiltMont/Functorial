bc6aa7d8a6f07dfc676ac54abf353c24dae79729
MiltMont <miltgonmon@pm.me>
2023-04-24 01:08:38 -0600

    fix bug

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -2,14 +2,45 @@ import { getAllArticlesFrontMatter, getArticleBySlug } from "../../lib/mdx";
 import { GetStaticPaths } from "next";
 import { MDXRemote } from "next-mdx-remote";
 import MDXComponents from "../../MDX/MDXComponents";
-import Layout from "../../components/Layout";
 import Spacer from "../../components/utils/Spacer";
-import theme from "../../styles/Theme";
-import { useWindowSize } from "../../hooks/useWindowSize";
-
-import S from "./css";
-
+import styled from "styled-components";
+import { base } from "../../utils/base";
+import Image from "next/image";
+import Layout from "../../components/Layout";
 import * as AspectRatio from "@radix-ui/react-aspect-ratio";
+
+const S = {
+  Container: styled.div`
+    @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
+      border: 1px solid ${(p) => p.theme.colors.accent3};
+      border-radius: ${(p) => p.theme.borderRadius[2]};
+
+      background-color: ${(p) => p.theme.colors.background};
+    }
+  `,
+
+  Root: styled.div`
+    @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
+      background-color: ${(p) => p.theme.colors.accent1};
+      padding: ${base(1 / 2)};
+    }
+  `,
+
+  ArticleImage: styled(Image)`
+    @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
+      border-radius: ${(p) => p.theme.borderRadius[2]};
+    }
+  `,
+
+  ImageContainer: styled(Layout.Container)`
+    @media (min-width: ${(p) => p.theme.breakpoints.sm}) {
+      padding: 100px;
+      background-color: ${(p) => p.theme.colors.accent1};
+      border: 10px solid ${(p) => p.theme.colors.accent3};
+      margin: 0;
+    }
+  `,
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getAllArticlesFrontMatter();
