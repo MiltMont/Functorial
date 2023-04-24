51dd6f6f000347a1bcea053069b96cc600af0085
MiltMont <miltgonmon@pm.me>
2023-04-23 23:51:46 -0600

    feat: Command menu detects if the page is an article and gives the option to share o like the article

--- components/CommandMenu/index.tsx
+++ components/CommandMenu/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { Global } from "../../utils/globals";
 import { useNavState } from "../../hooks/useNavState";
 import Spacer from "../utils/Spacer";
+import Router from "next/router";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,16 +14,33 @@ const inter = Inter({
 const CommandMenu = () => {
   const closed = useNavState((state) => state.closed);
   const setClosed = useNavState((state) => state.setClosed);
+
+  // Routing
+  const route = Router.route;
+
+  console.log(route.includes("/articles"));
+
   return (
     <S.Container>
       <Command label="Command Menu" className={inter.className}>
         <Command.List>
           <Command.Empty>No results found.</Command.Empty>
+
+          {route.includes("/articles") && (
+            <>
+              <Spacer />
+              <Command.Separator>Article</Command.Separator>
+              <Command.Item>Like article</Command.Item>
+              <Command.Item>Share article</Command.Item>
+            </>
+          )}
+
           <Spacer />
           <Command.Separator>Commands</Command.Separator>
           <Command.Item onSelect={() => setClosed(closed)}>
             Toggle Sidebar
           </Command.Item>
+
           <Spacer />
           <Command.Separator>Suggested</Command.Separator>
           {Global.navigation.map((item, i) => (
