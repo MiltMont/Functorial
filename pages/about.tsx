9d425ad1f21097d5b4f9324c7688b566f949b504
MiltMont <miltgonmon@pm.me>
2023-04-26 20:24:29 -0600

    test: still testing vercel integration

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,17 +2,42 @@ import Spacer from "../components/utils/Spacer";
 import Text from "../components/utils/Text";
 import Layout from "../components/Layout";
 import Section from "../components/utils/Section";
+import { GetStaticProps } from "next";
+import getPosts from "./api/posts";
 
-export default function About({ posts }) {
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      totalDocs: posts.totalDocs,
+      posts: posts.docs,
+    },
+  };
+};
+
+export default function About({ posts, totalDocs }) {
   return (
-    <Section bB>
-      <Spacer size={2} />
+    <>
+      <Section bB>
+        <Spacer size={2} />
+        <Layout.Container>
+          <Text as="h1" size={7} weight={4}>
+            About Me.
+          </Text>
+        </Layout.Container>
+        <Spacer size={2} />
+      </Section>
       <Layout.Container>
-        <Text as="h1" size={7} weight={4}>
-          About Me.
-        </Text>
+        <Spacer size={2} />
+        Here is some test:
+        <Spacer size={2} />
+        {posts.map((post) => (
+          <Text as="h1" size={7} weight={4} key={post.id}>
+            {post.title}
+          </Text>
+        ))}
       </Layout.Container>
-      <Spacer size={2} />
-    </Section>
+    </>
   );
 }
--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -2,9 +2,18 @@ import axios from "axios";
 
 const url = `http://${process.env.PAYLOAD_IP}:3000/api/posts`;
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   const r = await axios.get(url);
   const posts = r.data;
 
   res.status(200).json({ docs: posts.docs, totalDocs: posts.totalDocs });
+
+  return res.json({ docs: posts.docs, totalDocs: posts.totalDocs });
+}
+
+export default async function getPosts() {
+  const r = await axios.get(url);
+  const posts = r.data;
+
+  return { docs: posts.docs, totalDocs: posts.totalDocs };
 }
